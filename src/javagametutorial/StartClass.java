a8422a1676dcba711b57982b33b9e1bc693fd137
Todd Gibson <todd@ballistiksoftware.com>
2013-03-30 16:10:06 -0400

    initial commit

---
+++ src/javagametutorial/StartClass.java
@@ -0,0 +1,33 @@
+package javagametutorial;
+
+import java.applet.Applet;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class StartClass extends Applet implements Runnable, KeyListener {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
